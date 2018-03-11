5c016cca6f4a3c53ba6c60e0c83981fa2b4e3359
jvo203 <chris.zapart@nao.ac.jp>
2018-03-11 23:46:24 +0900

    an update

--- subarud.go
+++ subarud.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "github.com/kataras/iris"
 
 func main() {
 	fmt.Printf("SubaruWebQL HTTP/WebSocket daemon\n")
+
+	app := iris.New()
+
+	/*app.Get("/", func(ctx iris.Context) {
+		ctx.HTML("<H1>SubaruWebQL HTTP/WebSocket Server</H1>")
+	})*/
+
+	app.StaticWeb("/subaruwebql", "./htdocs/subaruwebql")
+	app.StaticWeb("/", "./htdocs/subaru.html")
+	app.Favicon("./htdocs/favicon.ico")
+	
+	// Start the server using a network address.
+	app.Run(iris.Addr(":8081"))
 }
